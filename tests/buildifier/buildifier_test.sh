359c72964eb880657380ebe073c430cb67c0af0c
Chris Brown <77508021+peakschris@users.noreply.github.com>
2024-06-30 11:52:13 -0400

    fix: enable norunfiles mode for buildifier_test

--- tests/buildifier/buildifier_test.sh
+++ tests/buildifier/buildifier_test.sh
@@ -1,4 +1,34 @@
-source ./tests/unittest/unittest.bash || exit 1
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v3 ---
+# Copy-pasted from the Bazel Bash runfiles library v3.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v3 ---
+
+# return a unix-style path on all platforms
+# workaround for https://github.com/bazelbuild/bazel/issues/22803
+function rlocation_as_unix() {
+  path=$(rlocation ${1})
+  case "$(uname -s)" in
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    path=${path//\\//} # backslashes to forward
+    path=/${path//:/}  # d:/ to /d/
+    ;;
+  esac
+  echo $path
+}
+
+# MARK - Locate Deps
+
+unittest_bash_location=_main/tests/unittest/unittest.bash
+unittest_bash="$(rlocation_as_unix "${unittest_bash_location}")"
+source ${unittest_bash} || exit 1
 
 function create_bazelrc() {
   cat >testws/.bazelrc << EOF
